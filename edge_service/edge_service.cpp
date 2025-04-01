b15014efab4a7ae78da9d19287aa7490a8d73814
realht <realht@gmail.com>
2025-04-01 22:34:50 +0300

    async incr rate to 6k+

--- edge_service/edge_service.cpp
+++ edge_service/edge_service.cpp
@@ -11,46 +11,51 @@
 #include <string>
 #include <atomic>
 #include <csignal>
+#include <sys/resource.h>
 
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
-using grpc::ServerReader;
+using grpc::ServerAsyncReader;
+using grpc::CompletionQueue;
+using grpc::ServerCompletionQueue;
 using grpc::Status;
 using iot::v1::SensorData;
 using iot::v1::Ack;
 using iot::v1::EdgeService;
 
+namespace supp_var{
+	const std::string REDIS_SERV_ADRESS = "tcp://localhost:6379";
+	const std::string EGDE_SERV_ADDRESS = "0.0.0.0:50051";
+    constexpr size_t ASYNC_INTERVAL_MS = 10;
+    constexpr size_t MAX_CONCURRENT_STREAMS = 1000000;
+}
+
 //глобальный флаг для graceful shutdown
 std::atomic<bool> shutdown_requested{false};
 
-//обработчик сигналов SIGINT и SIGTERM
-void signal_handler(int signum){
-	std::cout <<"\nSignal (" << signum<< ") recived. Initiating graceful shutdown...\n";
-	shutdown_requested.store(true);
-}
-
 //потокобезопасная очередь для SensorData
-class TreadSafeQueue{
+template <typename T>
+class ThreadSafeQueue{
 public:
-	void push(const SensorData& data){
+	void push(const T& value){
 		std::lock_guard<std::mutex> lock(mutex_);
-		sd_queue_.push(data);
+		queue_.push(value);
 		cond_var_.notify_one();
 	}
 
 	//пытается извлечь данные. если очередь пуста блокируется до появления данных
-	SensorData pop(){
+	bool pop(T& value){
 		std::unique_lock<std::mutex> lock(mutex_);
-		cond_var_.wait(lock, [this]() {return !sd_queue_.empty() || shutdown_;} );
-		if(sd_queue_.empty()){
-			//если очередь пуста возвращает пустой объект
-			return SensorData();
-		}
-		SensorData data = sd_queue_.front();
-		sd_queue_.pop();
-		return data;
+		cond_var_.wait(lock, [this]() {return !queue_.empty() || shutdown_;} );
+		
+		//если очередь пуста возвращает пустой объект
+		if(queue_.empty()) return false;
+
+		value = std::move(queue_.front());
+		queue_.pop();
+		return true;
 	}
 
 	//сигнализирует о завершении работы очереди
@@ -60,54 +65,24 @@ public:
 		cond_var_.notify_all();
 	}
 
-	bool is_shutdown(){
-		return shutdown_;
-	}
-
 private:
-	std::queue<SensorData> sd_queue_;
+	std::queue<T> queue_;
 	mutable std::mutex mutex_;
 	std::condition_variable cond_var_;
 	bool shutdown_ = false;
 };
 
 
-class EdgeServiceImpl final : public EdgeService::Service{
+class EdgeServiceImpl final : public EdgeService::AsyncService{
 public:
-
-	EdgeServiceImpl()
-	: redis_(std::make_unique<sw::redis::Redis>("tcp://localhost:6379"))
-		{
-		// Проверка подключения к Redis
-        try {
-            redis_->ping();
-            std::cout << "Connected to Redis successfully\n";
-        } catch (const std::exception& e) {
-            std::cerr << "Redis connection error: " << e.what() << "\n";
-            exit(1);
-        }
-		for (int i = 0; i < std::thread::hardware_concurrency(); ++i) {
-			workers_.emplace_back([this](std::stop_token st){
-				//каждый поток непрерывно обрабатывает очередь
-				while(!st.stop_requested() && !ts_queue_.is_shutdown()){
-					SensorData data = ts_queue_.pop();
-					if(data.device_id().empty()){
-						continue;
-					}
-					std::vector<std::pair<std::string, std::string_view>> entires{
-						{"device_id", data.device_id()},
-						{"temperature", std::to_string(data.temperature())},
-						{"humidity", std::to_string(data.humidity())},
-						{"timestamp", std::to_string(data.timestamp().seconds())}};
-					try{
-						redis_->xadd(
-							"telemetry_stream",
-							"*",
-							entires.begin(), entires.end());
-						redis_->expire("telemetry_stream",3600*24);
-					}catch(const std::exception& e){
-						std::cerr << "Error writing to Redis: " << e.what() << '\n';
-					}	
+	explicit EdgeServiceImpl(size_t thread_pool_size)
+	: redis_pool_(thread_pool_size){
+		// запуск потоков
+		for(size_t i = 0;i < thread_pool_size;i++){
+			workers_.emplace_back([this](std::stop_token st) {
+				SensorData data;
+				while(!st.stop_requested()){
+					if(queue_.pop(data)) processSensorData(data);
 				}
 			});
 		}
@@ -115,59 +90,183 @@ public:
 
 	~EdgeServiceImpl(){
 		//сигнал рабочим потокам завершить работу
-		ts_queue_.shutdown();
+		queue_.shutdown();
 	}
 
-	Status StreamTelemetry(ServerContext* context, 
-		ServerReader<SensorData>* reader, Ack* ack) override{
-			SensorData data;
-			while(reader->Read(&data)){
-				//write to console for test only
-				std::cout << "Recivind data from device " << data.device_id() <<
-				": Temperature=" << data.temperature() << ", Humidity=" <<
-				data.humidity() << '\n';
-				ts_queue_.push(data);
+	void StartPrecessing(ServerCompletionQueue* cq){
+		new CallData(this, cq);
+	}
+
+	
+private:
+	friend void RunServer();
+
+	class CallData{
+		public:
+		CallData(EdgeServiceImpl* service, ServerCompletionQueue* cq)
+			: service_(service), cq_(cq), responder_(&ctx_), status_(CREATE)
+			{Proceed();}
+
+		
+		void Proceed(bool ok = true) {
+			if (status_ == CREATE) {
+				status_ = PROCESSING;
+				service_->RequestStreamTelemetry(&ctx_, &responder_, cq_, cq_, this);
+			} else if (status_ == PROCESSING) {
+				if (shutdown_requested.load()) {  // Проверка завершения
+					delete this;
+					return;
+				}
+				// Новый запрос принят, создаем новый CallData для следующего запроса
+				new CallData(service_, cq_);
+				status_ = READING;
+				responder_.Read(&request_, this);
+				///std::cout << "data processed\n";				
+			} else if (status_ == READING) {
+				if (!ok) {
+					// Клиент завершил передачу, завершаем поток
+					status_ = FINISHED;
+					ack_.set_success(true);
+					ack_.set_message("Stream processed asyncly");
+					responder_.Finish(ack_, Status::OK, this);
+					return;
+				} 
+				// Данные прочитаны, отправляем в очередь
+				service_->queue_.push(request_);
+				// Продолжаем читать следующее сообщение
+				responder_.Read(&request_, this);
+			} else {
+				delete this;
 			}
-			ack->set_success(true);
-			ack->set_message("Data processing initiaded async");
-			return Status::OK;
 		}
 
-private:
-	std::unique_ptr<sw::redis::Redis> redis_;
-	TreadSafeQueue ts_queue_; 
+
+		private:
+			EdgeServiceImpl* service_;
+			ServerCompletionQueue* cq_;
+			ServerContext ctx_;
+			ServerAsyncReader<Ack, SensorData> responder_;
+			SensorData request_;
+			Ack ack_;
+			enum CallStatus{CREATE, PROCESSING, READING,FINISHED};
+			CallStatus status_;
+	};
+
+	class RedisPool{
+		public:
+		RedisPool(size_t size){
+			for(size_t i=0;i<size;i++){
+				pool_.emplace_back(std::make_shared<sw::redis::Redis>(
+					supp_var::REDIS_SERV_ADRESS));
+
+			}
+		}
+
+		std::shared_ptr<sw::redis::Redis> get(){
+			// static std::atomic<size_t> index{0};
+			// return pool_[index++ % pool_.size()];
+			thread_local static size_t index = 0;
+			return pool_[index++ % pool_.size()];
+		}
+
+		private:
+			std::vector<std::shared_ptr<sw::redis::Redis>> pool_;
+
+	};
+
+	void processSensorData(const SensorData& data){
+		try{
+			auto redis = redis_pool_.get();
+						
+			std::vector<std::pair<std::string, std::string>> entires{
+				{"device_id", data.device_id()},
+				{"temperature", std::to_string(data.temperature())},
+				{"humidity", std::to_string(data.humidity())},
+				{"timestamp", std::to_string(data.timestamp().seconds())}};
+			redis->xadd("telemetry_stream", "*",
+					entires.begin(),entires.end());
+			//std::cout << "data: " << entires.front().second<< '\n';
+		}catch(const std::exception& e){
+			std::cerr << "Error writing to Redis: " << e.what() << '\n';
+		}
+	}
+
+	ThreadSafeQueue<SensorData> queue_;
+	RedisPool redis_pool_;
 	std::vector<std::jthread> workers_;
 };
 
 void RunServer(){
-	std::string server_address("0.0.0.0:50051");
-
-	EdgeServiceImpl service;
+	EdgeServiceImpl service(std::thread::hardware_concurrency());
 	ServerBuilder builder;
-	builder.AddListeningPort(server_address,grpc::InsecureServerCredentials());
+
+	//настройка асинхронного сервера и добавляем компрессию
+	builder.AddListeningPort(supp_var::EGDE_SERV_ADDRESS,grpc::InsecureServerCredentials());
 	builder.RegisterService(&service);
-	std::unique_ptr<Server> server(builder.BuildAndStart());
-	std::cout << "Edge server listining on " << server_address << '\n';
+	builder.SetDefaultCompressionAlgorithm(GRPC_COMPRESS_GZIP);
+
+	auto cq = builder.AddCompletionQueue();
+	auto server = builder.BuildAndStart();
+	service.StartPrecessing(cq.get());
+
+	std::cout << "Edge server v3 listining on " << supp_var::EGDE_SERV_ADDRESS << '\n';
+
+	//Поток для обработки GRPC
+	std::jthread cq_thread([cq = cq.get()](std::stop_token st){
+		void* tag;
+		bool ok;
+		constexpr auto poll_interval = std::chrono::milliseconds(supp_var::ASYNC_INTERVAL_MS);
+
+		while(!st.stop_requested()){
+			auto deadline = std::chrono::system_clock::now() + poll_interval;
+			auto timeout = std::chrono::duration_cast<std::chrono::milliseconds>(
+				deadline - std::chrono::system_clock::now());
+
+			CompletionQueue::NextStatus status = cq->AsyncNext(
+				&tag, &ok, gpr_time_add(
+					gpr_now(GPR_CLOCK_MONOTONIC),
+					gpr_time_from_millis(timeout.count(),GPR_TIMESPAN))
+			);
+
+			if(status == grpc::CompletionQueue::GOT_EVENT){
+				if(tag) static_cast<EdgeServiceImpl::CallData*>(tag)->Proceed(ok);
+			}else if(status == grpc::CompletionQueue::SHUTDOWN){
+				break;
+			}
+		}
+	});
+
 
 	//поток для grace shutdown сервера
 	std::jthread shutdown_thread([&](std::stop_token st){
-		while(!st.stop_requested() && !shutdown_requested.load()){
+		while(!st.stop_requested()){
+			if(shutdown_requested.load()){
+				std::cout << "Shutdown signal detected. Shutting down EdgeServer gracefully...\n";
+				server->Shutdown();
+				cq->Shutdown();
+				break;
+			}
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
-		}
-		if(shutdown_requested.load()){
-			std::cout << "Shutdown signal detected. Shutting down EdgeServer gracefully...\n";
-			server->Shutdown();
 		}
 	});
 
 	//ожидаем завершение работы сервера
 	server->Wait();
+
+	//очищение очереди
+	void* tag;
+	bool ok;
+	while (cq->Next(&tag, &ok)) {
+		if (tag) {
+		static_cast<EdgeServiceImpl::CallData*>(tag)->Proceed(ok);
+		}
+	}
 }
 
 int main(){
 	//регистрируем обработчики сигналов для SIGINT и SIGTERM
-	std::signal(SIGINT, signal_handler);
-	std::signal(SIGTERM, signal_handler);
+	std::signal(SIGINT, [](int){shutdown_requested.store(true);});
+	std::signal(SIGTERM, [](int){shutdown_requested.store(true);});
 
 	std::cout << "Greating to async grpc req server\n";
 	RunServer();
